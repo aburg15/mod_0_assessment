7e11900db3176ed5b1288d14d76101b612fe712c
Adam Burgess <aburg1515@gmail.com>
2021-07-30 09:20:53 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var animal = 'zebra';
+var isCuddly = true;
+animal = 'bear';
+var num8 = 8;
+var num3 = 3;
+var quotient = num3 / num8;
+var favoriteFilms = ['zoolander', 'mad max', 'dodgeball'];
+favoriteFilms.shift();
+console.log(animal.length);
