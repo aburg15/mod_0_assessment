2d5be6a3e5105db33d4b4a746c78588fe904749d
Adam Burgess <aburg1515@gmail.com>
2021-07-30 09:37:23 -0600

    add house variable using an object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+  houseLocation: 'Denver, CO',
+  houseStreetName: 'Weeping Willow Dr.',
+  numberOfBedrooms: 3,
+  numberOfBathrooms: 2,
+  isHaunted: true,
+  isCloseToSchool: false,
+  neighborsNames: ['john', 'sally', 'david'],
+  nearbyStores: ['best buy', 'target', 'starbucks'],
+}
