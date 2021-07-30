eb72c4cf00d6eff2070c9231813a84fcbf4a2646
Adam Burgess <aburg1515@gmail.com>
2021-07-30 09:31:01 -0600

    add laptop variable using an object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var laptop = {
+  laptopBrand: 'MacBook Pro',
+  laptopColor: 'silver',
+  yearManufactured: 2020,
+  yearReceived: 2021,
+  isExpensive: true,
+  isSlow: false,
+  laptopApplications: ['chrome', 'spotify', 'rectangle'],
+  laptopDirectories: ['applications', 'documents', 'downloads'],
+}
